3c039239e5a17587679662c15a654cd444f394e3
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-09-17 21:30:24 -0600

    Swiper parte 1

--- lib/presentation/screens/movies/home_screem.dart
+++ lib/presentation/screens/movies/home_screem.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -30,15 +31,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayinMoviesProvider);
-    if (nowPlayingMovies.length == 0) return const CircularProgressIndicator();
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
