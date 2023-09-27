1909f86606f63c5bb1e35e5fd18abd13cca1204e
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-09-27 00:46:20 -0600

    Escuchar múltiples providers simultáneamente

--- lib/presentation/screens/movies/home_screem.dart
+++ lib/presentation/screens/movies/home_screem.dart
@@ -34,15 +34,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayinMoviesProvider);
+    final initialLoading = ref.watch(initialLoadingProvider);
+
+    if (initialLoading) return const FullScreenLoader();
+
     final slideShowMovies = ref.watch(mociesSlideshowProvider);
 
-
+    final nowPlayingMovies = ref.watch(nowPlayinMoviesProvider);
     final popuparMovies = ref.watch(popuparMoviesProvider);
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-    return FullScreenLoader();
 
     return CustomScrollView(slivers: [
       const SliverAppBar(
@@ -52,8 +53,6 @@ class _HomeViewState extends ConsumerState<_HomeView> {
           titlePadding: EdgeInsets.all(0),
         ),
       ),
-
-
       SliverList(
           delegate: SliverChildBuilderDelegate((context, index) {
         return Column(
