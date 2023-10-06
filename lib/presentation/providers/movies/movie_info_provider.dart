8503902213cd923569140fbe16451b9be8913ee2
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-10-05 23:38:32 -0600

    Realizar la petición HTTP y probar caché

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -5,7 +5,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../domain/entities/movie.dart';
 
-final movieInfoProvider= StateNotifierProvider((ref){
+final movieInfoProvider= StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref){
 
     final movieRepository  = ref.watch(movieRepostoryProvider);
 
@@ -23,7 +23,7 @@ MovieMapNotifier({required this.getMovie}): super({});
 
 Future<void> loadMovie(String movieId) async {
   if(state[movieId] !=null) return;
-
+  print('Reaalizando Http');
   final movie = await getMovie(movieId);
 
   state ={...state, movieId: movie};
