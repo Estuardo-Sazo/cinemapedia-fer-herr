42ff2c814c84c3b162554a08dd123c3ade488a81
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-10-06 00:02:30 -0600

    Diseño de la pantalla de película

--- lib/presentation/screens/movies/movie_screem.dart
+++ lib/presentation/screens/movies/movie_screem.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +30,7 @@ class MovieScreemState extends ConsumerState<MovieScreem> {
 
     if (movie == null) {
       return const Scaffold(
-        body:  Center(
+        body: Center(
           child: CircularProgressIndicator(
             strokeWidth: 2,
           ),
@@ -37,8 +39,70 @@ class MovieScreemState extends ConsumerState<MovieScreem> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(
+            movie: movie,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliverAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      shadowColor: Colors.red,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(children: [
+          SizedBox.expand(
+            child: Image.network(
+              movie.posterPath,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7, 1.0],
+                    colors: [
+                Colors.transparent,
+                Colors.black87,
+              ])),
+            ),
+          ),
+          const SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.3],
+                    colors: [
+                Colors.black87,
+                Colors.transparent,
+              ])),
+            ),
+          )
+        ]),
       ),
     );
   }
