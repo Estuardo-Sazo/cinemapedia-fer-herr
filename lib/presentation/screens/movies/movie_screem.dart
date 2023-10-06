3ec34ea3307244ef6825980d708f555638217d79
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-10-06 00:38:44 -0600

    Descripción de la película

--- lib/presentation/screens/movies/movie_screem.dart
+++ lib/presentation/screens/movies/movie_screem.dart
@@ -42,11 +42,82 @@ class MovieScreemState extends ConsumerState<MovieScreem> {
       body: CustomScrollView(
         physics: const ClampingScrollPhysics(),
         slivers: [
-          _CustomSliverAppBar(
-            movie: movie,
-          ),
+          _CustomSliverAppBar(movie: movie),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => _MovieDetails(movie: movie),
+                  childCount: 1))
         ],
       ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(
+                    movie.title,
+                    style: textStyles.titleLarge,
+                  ),
+                  Text(movie.overview),
+                ]),
+              ),
+            ],
+          ),
+        ),
+
+//TODO Generos de peli
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(gender),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+//TODO Mostrar actores
+//TODO Mostrar actores
+        const SizedBox(
+          height: 100,
+        ),
+      ],
     );
   }
 }
@@ -81,22 +152,25 @@ class _CustomSliverAppBar extends StatelessWidget {
             child: DecoratedBox(
               decoration: BoxDecoration(
                   gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1.0],
-                    colors: [
-                Colors.transparent,
-                Colors.black87,
-              ])),
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [
+                    0.7,
+                    1.0
+                  ],
+                      colors: [
+                    Colors.transparent,
+                    Colors.black87,
+                  ])),
             ),
           ),
           const SizedBox.expand(
             child: DecoratedBox(
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.3],
-                    colors: [
+                  gradient: LinearGradient(begin: Alignment.topLeft, stops: [
+                0.0,
+                0.3
+              ], colors: [
                 Colors.black87,
                 Colors.transparent,
               ])),
