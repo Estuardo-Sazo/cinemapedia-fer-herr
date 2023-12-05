a45e10933a058d0a0a3c1cc6a0b3469d3f306e2c
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-12-04 22:16:50 -0600

    Errores nulos

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -17,8 +17,9 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
-      releaseDate: moviedb.releaseDate,
+          : 'https://genesisairway.com/wp-content/uploads/2019/05/no-image.jpg',
+      releaseDate:
+          moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -2,12 +2,20 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovie;
+
+  SearchMovieDelegate({
+    required this.searchMovie,
+  });
+
   @override
   String get searchFieldLabel => 'Buscar pelicula';
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return  [
+    return [
       if (query.isNotEmpty)
         FadeIn(
           child: IconButton(
@@ -35,7 +43,55 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    return const Text('Buils Search Suggestions');
+    if (query.isEmpty) {
+      return const Center(
+        child: Text('Busca una pelicula'),
+      );
+    }
+    return FutureBuilder<List<Movie>>(
+      future: searchMovie(query),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error al buscar'),
+          );
+        }
+        if (snapshot.hasData) {
+          final movies = snapshot.data!;
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return ListTile(
+                onTap: () {
+                  close(context, movie);
+                },
+                leading: FadeInLeft(
+                  child: Hero(
+                    tag: movie.id,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: FadeInImage(
+                        placeholder: const AssetImage('assets/no-image.jpg'),
+                        image: NetworkImage(movie.posterPath),
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                title: FadeInRight(
+                  child: Text(movie.title),
+                ),
+              );
+            },
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
