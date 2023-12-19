79a6a7f0e35f9904ff0bb99276dab1df9b7a0468
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-12-19 00:26:41 -0600

    ShellRoute - GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,9 +3,38 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screem.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/', 
+  initialLocation: '/',
   routes: [
-  GoRoute(
+    ShellRoute(
+        builder: (context, state, child) {
+          return HomeScreem(childView: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreem.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreem(
+                    movieId: movieId,
+                  );
+                },
+              )
+            ],
+            
+          ),
+          GoRoute(
+              path: '/favorite',
+              builder: (context, state) => const FavoriteView()),
+        ])
+
+    /* GoRoute(
       path: '/',
       name: HomeScreem.name,
       builder: (context, state) => const HomeScreem(
@@ -22,5 +51,6 @@ final appRouter = GoRouter(
             );
           },
         )
-      ]),
-]);
+      ]), */
+  ],
+);
